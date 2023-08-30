59d913fa8f6cc33dd589578ba2302d0333268915
Igor Duarte <igor.duartematos@gmail.com>
2023-08-30 14:03:20 -0300

    feat: Muda role admin

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -8,7 +8,7 @@ namespace smart_dental_webapi.Repositories
         {
             var users = new List<UserEntity>
             {
-                new UserEntity { Id = 1, Username = "admin", Password = "admin", Role = "manager" }
+                new UserEntity { Id = 1, Username = "admin@admin.com", Password = "admin@", Role = "manager" }
             };
 
             return users.FirstOrDefault(x => x.Username.ToLower() == username.ToLower() && x.Password.ToLower() == password.ToLower());
