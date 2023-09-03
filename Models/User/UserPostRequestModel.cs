4539accbfd553a03449de62f40cc2dcf50405eec
Igor Duarte <igor.duartematos@gmail.com>
2023-09-03 10:06:57 -0300

    Remove files/folder

--- Models/User/UserPostRequestModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace smart_dental_webapi.Models.User
-{
-    public class UserPostRequestModel
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
--- Settings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace smart_dental_webapi
-{
-    public class Settings
-    {
-        public static string Secret = "419c2edc1711aadc3e59d88d3d4a52cb";
-    }
-}
